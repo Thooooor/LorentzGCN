d44a436b342c6ca7ff04731d7bb19697cdf1f861
zhihao <lizhihao2022@163.com>
2023-03-27 00:36:39 +0800

    change default k_list

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ parser.add_argument("--mode", type=str, choices=["train", "valid", "test"], defa
 parser.add_argument("--model", type=str, default="light_gcn")
 parser.add_argument("--layer", type=int, default=3, help="layer num of GCN")
 parser.add_argument("--dim", type=int, default=64, help='dimensionality of atom features')
-parser.add_argument("--k_list", default="[20, 50]")
+parser.add_argument("--k_list", default="[50]")
 # Log Parameters
 parser.add_argument("--log", type=bool, default=True, help="enable logging")
 parser.add_argument("--log_dir", type=str, default="./logs", help="Dir for logs")
@@ -20,12 +20,12 @@ parser.add_argument("--device", type=int, default=0)
 parser.add_argument("--epochs", type=int, default=500)
 parser.add_argument("--patience", type=int, default=-1)
 parser.add_argument("--eval_freq", type=int, default=5)
-parser.add_argument("--batch_size", type=int, default=256, help='mini-batch size')
+parser.add_argument("--batch_size", type=int, default=512, help='mini-batch size')
 parser.add_argument("--sample_size", type=int, default=-1, help="sample size, -1 to not use sampling")
 # Optimizer Parameters
 parser.add_argument("--optimizer", type=str, choices=["Adagrad", "Adam", "SGD"], default='Adam')
 parser.add_argument("--lr", type=float, default=1e-3)
-parser.add_argument("--weight_decay", type=float, default=0)
+parser.add_argument("--weight_decay", type=float, default=0.001)
 parser.add_argument("--momentum", type=float, default=0.9)
 # Dataset Parameters
 parser.add_argument("--data_dir", type=str, default="./data")
