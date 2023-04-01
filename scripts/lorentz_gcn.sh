cb92cabb3207812075af848f5162d1950c0a1388
Zhihao <lizhihao2022@163.com>
2023-04-01 20:58:52 +0800

    add scripts for training

---
+++ scripts/lorentz_gcn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python main.py \
+    --model lorentz_gcn \
+    --layers 3 \
+    --dim 64 \
+    --scale 0.1 \
+    --margin 1.0 \
+    --k_list [50] \
+    --random_seed 2020 \
+    --cuda True \
+    --device 4 \
+    --epochs 500 \
+    --patience 5 \
+    --eval_freq 5 \
+    --batch_size 1024 \
+    --optimizer rsgd \
+    --lr 0.001 \
+    --weight_decay 0.001 \
+    --momentum 0.8 \
